48deb4d1c721c118f97323bc762c1e675f6be4eb
Ramsharan Gajurel <ramsharangajurel9742@gmail.com>
2026-01-02 20:33:11 +0545

    Updated

--- portfolio/src/components/Navbar.jsx
+++ portfolio/src/components/Navbar.jsx
@@ -1,24 +1,86 @@
-import React from 'react'
-import {NavLink} from 'react-router-dom';
+import React, { useState } from "react";
+import { Link } from "react-router-dom";
+import { FaSearch, FaBars } from "react-icons/fa";
 
-const Navbar = () => {
+export default function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+  const [closing, setClosing] = useState(false);
+
+  const handleNavItemClick = () => {
+    setClosing(true);
+    setTimeout(() => {
+      setMenuOpen(false);
+      setClosing(false);
+    }, 300);
+  };
+
   return (
-    <div className='Navbar'>
-        <div className='left-nav'>
-
-        
+    <>
+    
+      <nav className="main-navbar">
+        <div className="nav-container">
+          <div className="nav-left">
+            <button
+              className="menu-toggle"
+              onClick={() => setMenuOpen(!menuOpen)}
+            >
+              <FaBars />
+            </button>
+            <ul
+              className={`nav-links ${menuOpen ? "active" : ""} ${
+                closing ? "slide-up" : ""
+              }`}
+            >
+              <li>
+                <Link to="/" className="nav-link" onClick={handleNavItemClick}>
+                  Home
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/about"
+                  className="nav-link"
+                  onClick={handleNavItemClick}
+                >
+                  About us
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/projects"
+                  className="nav-link"
+                  onClick={handleNavItemClick}
+                >
+                  Projects
+                </Link>
+              </li>
+               <li>
+                <Link
+                  to="/skills"
+                  className="nav-link"
+                  onClick={handleNavItemClick}
+                >
+                  Skills
+                </Link>
+              </li>
+              <li>
+                <Link
+                  to="/contact"
+                  className="nav-link"
+                  onClick={handleNavItemClick}
+                >
+                  Contact
+                </Link>
+              </li>
+            
+            </ul>
+          </div>
+          <div className="nav-right">
+            <FaSearch className="search-icon" />
+              <Link to="/appointment" className="btn-appointment">Resume</Link>
+          </div>
         </div>
-         <ul className="right-nav">
-           <li><NavLink to="/">Home</NavLink></li>
-           <li><NavLink to="/about">About</NavLink></li>
-           <li><NavLink to="/skills">Skills</NavLink></li>
-           <li><NavLink to="/projects">Projects</NavLink></li>
-           <li>
-            <NavLink to="/contact" className="contact-btn">Contact</NavLink>
-           </li>
-      </ul>
-    </div>
-  )
+      </nav>
+    </>
+  );
 }
-
-export default Navbar
