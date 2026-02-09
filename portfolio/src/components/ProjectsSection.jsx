94750f24b82e31ec6ca21a1e4749a0e71b6dce7b
Ram Sharan Gajurel <ramsharangajurel9742@gmail.com>
2026-02-09 21:50:25 +0545

    Updated

--- portfolio/src/components/ProjectsSection.jsx
+++ portfolio/src/components/ProjectsSection.jsx
@@ -1,32 +1,44 @@
 import ProjectCard from "./ProjectCard";
-import PortfolioImg from "../assets/portfolio.png"
-import HospitalImg from "../assets/hospital.png"
-import BookImg from "../assets/bookstore.png"
+import PortfolioImg from "../assets/portfolio.png";
+import HospitalImg from "../assets/hospital.png";
+import BookImg from "../assets/bookstore.png";
 // Sample project data
 const projects = [
   {
     title: "Portfolio Website",
-    description: "A personal portfolio website showcasing my projects and skills with animations and dark mode.",
+    description:
+      "A personal portfolio website showcasing my projects and skills with animations and dark mode.",
     tech: ["React", "Framer Motion", "CSS"],
     image: PortfolioImg,
     live: "https://yourportfolio.com",
-    repo: "https://github.com/username/portfolio"
+    repo: "https://github.com/username/portfolio",
   },
   {
     title: "Hospital Webpage",
-    description: "Full-stack  app with user authentication, book appointment, get news as well as other various features.",
+    description:
+      "Full-stack  app with user authentication, book appointment, get news as well as other various features.",
     tech: ["React", "Node.js", "MongoDB", "Express"],
     image: HospitalImg,
     live: "https://meddical.netlify.app/",
-    repo: "https://github.com/ramsharangajurel-31/MEDDICAL_INTERN_WEBSITE"
+    repo: "https://github.com/ramsharangajurel-31/MEDDICAL_INTERN_WEBSITE",
   },
   {
     title: "Online BookStore",
-    description: "A modern  platform to buy books as well as read bokks / admin Panel features to login up/ siggn up",
-    tech: ["React", "Node.js", "Express", "MongoDB" , "Vite", "Multer" , "Auth"],
+    description:
+      "A modern  platform to buy books as well as read bokks / admin Panel features to login up/ siggn up",
+    tech: ["React", "Node.js", "Express", "MongoDB", "Vite", "Multer", "Auth"],
     image: BookImg,
     live: "https://minionlinebookstore.netlify.app/",
-    repo: "https://github.com/ramsharangajurel-31/Book_Store"
+    repo: "https://github.com/ramsharangajurel-31/Book_Store",
+  },
+  {
+    title: "Mini Job Portal",
+    description:
+      "Mini Job Portal is a web application that connects job seekers with employers. It features job listings with details, search and filter functionality, job bookmarking, and pagination for easy navigation.",
+    tech: ["React", "CSS", "React Icons", "Framer Motion"],
+    image: BookImg,
+    live: "https://minijobportal.netlify.app/",
+    repo: "https://github.com/ramsharangajurel-31/React-Task",
   },
 ];
 
